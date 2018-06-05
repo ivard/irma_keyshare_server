21332d2180e756866e544946cdaf19882a67010e
David Venhoek <david@venhoek.nl>
2018-06-05 12:11:06 +0200

    Fixed whitespace issues.

--- src/main/java/org/irmacard/keyshare/web/Users/User.java
+++ src/main/java/org/irmacard/keyshare/web/Users/User.java
@@ -343,10 +343,10 @@ public class User extends Model {
 	// http://stackoverflow.com/a/20984477
 	public static long pow(long a, int b)
 	{
-		if (b == 0)		return 1;
-		if (b == 1)		return a;
-		if (b%2 == 0)	  return	 pow (a * a, b/2); // even a=(a^2)^b/2
-		else			   return a * pow (a * a, b/2); // odd  a=a*(a^2)^b/2
+		if (b == 0)        return 1;
+		if (b == 1)        return a;
+		if (b%2 == 0)      return     pow (a * a, b/2); // even a=(a^2)^b/2
+		else               return a * pow (a * a, b/2); // odd  a=a*(a^2)^b/2
 	}
 
 	public boolean old() {
